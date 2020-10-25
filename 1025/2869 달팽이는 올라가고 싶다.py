f4c1d7f7a1f16a0ecc8508e38ba3073080648bb7
Swanius <ohsuwan1@gmail.com>
2020-10-26 00:46:24 +0900

    1026algo

---
+++ 1025/2869 달팽이는 올라가고 싶다.py
@@ -0,0 +1,9 @@
+'''
+3 1 10
+'''
+
+a, b, v = map(int, input().split())
+if (v - a) % (a - b) == 0:
+    print(((v - a) // (a - b)) + 1)
+else:
+    print(((v - a) // (a - b)) + 2)
--- 1025/4963 섬의 개수.py
+++ 1025/4963 섬의 개수.py
@@ -1,32 +1,5 @@
 from collections import deque
 
-'''
-1 1
-0
-2 2
-0 1
-1 0
-3 2
-1 1 1
-1 1 1
-5 4
-1 0 1 0 0
-1 0 0 0 0
-1 0 1 0 1
-1 0 0 1 0
-5 4
-1 1 1 0 1
-1 0 1 0 1
-1 0 1 0 1
-1 0 1 1 1
-5 5
-1 0 1 0 1
-0 0 0 0 0
-1 0 1 0 1
-0 0 0 0 0
-1 0 1 0 1
-0 0
-'''
 dy = [0, 0, 1, 1, 1, -1, -1, -1]
 dx = [1, -1, 0, -1, 1, 0, -1, 1]
 
@@ -35,9 +8,9 @@ def bfs(y, x):
     global cnt
 
     arr[y][x] = 0
-    for i in range(8):
-        ny = y + dy[i]
-        nx = x + dx[i]
+    for z in range(8):
+        ny = y + dy[z]
+        nx = x + dx[z]
         if ny < 0 or ny >= n or nx < 0 or nx >= m:
             continue
 
@@ -49,23 +22,24 @@ def bfs(y, x):
 
         if arr[ny][nx] == 1:
             q.append((ny, nx))
+            visit[ny][nx] = True
 
 while True:
-    q = deque()
     m, n = map(int, input().split())
     if m == 0 and n == 0:
         break
-    else:
-        arr = [list(map(int, input().split())) for _ in range(n)]
-        visit = [[False] * m for _ in range(n)]
-        cnt = 0
-        for i in range(n):
-            for j in range(m):
-                if (arr[i][j] == 1) and not visit[i][j]:
-                    visit[i][j] = True
-                    q.append((i, j))
-                    while q:
-                        bfs(*q.popleft())
-                    cnt += 1
 
-        print(cnt)
+    q = deque()
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    visit = [[False] * m for _ in range(n)]
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if (arr[i][j] == 1) and not visit[i][j]:
+                visit[i][j] = True
+                q.append((i, j))
+                while q:
+                    bfs(*q.popleft())
+                cnt += 1
+
+    print(cnt)
