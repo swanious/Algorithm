23038164c5634419f8326cbae6dc5ee7c489b4a2
Swanius <ohsuwan1@gmail.com>
2020-12-09 00:03:57 +0900

    boj 15886

--- 1208/[BOJ]15886 내 선물을 받아줘2.py
+++ 1208/[BOJ]15886 내 선물을 받아줘2.py
@@ -1,9 +1,8 @@
 n = int(input())
 arr = list(input())
 cnt = 0
-if arr[0] == 'W':
-    cnt += 1
-elif arr[-1] == 'E':
-    cnt += 1
-
-for i in range
+if arr[0] == 'W': cnt += 1
+elif arr[-1] == 'E': cnt += 1
+for i in range(len(arr)-1):
+    if arr[i]+arr[i+1] == 'EW': cnt += 1
+print(cnt)
