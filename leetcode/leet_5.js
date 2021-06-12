38cbe232394b57668fc654833928a8c369518cbf
swanious <ohsuwan1@gmail.com>
2021-06-13 02:06:09 +0900

    가장 긴 팰린드롬 with Manacher's algo

--- leetcode/leet_5.js
+++ leetcode/leet_5.js
@@ -2,11 +2,19 @@
  * @param {string} s
  * @return {string}
  */
+
+/*
+  가장 긴 팰린드롬 찾기
+
+  시간복잡도 O(n)으로 풀기 위해서, Manacher's algorithm을 활용하여 풀이를 한다.
+  http://www.secmem.org/blog/2019/03/10/Manacher/ 참고해보자.
+*/
 var longestPalindrome = function (s) {
   var i,
     len = Math.floor(s.length / 2) + 1,
     isTotalPalindrome = true;
 
+  // 문자열 그 자체가 팰린드롬인 경우 그냥 s를 return 한다.
   for (i = 0; i < len; i++) {
     if (s[i] != s[s.length - i - 1]) {
       isTotalPalindrome = false;
@@ -16,9 +24,12 @@ var longestPalindrome = function (s) {
 
   if (isTotalPalindrome) return s;
 
+  // manacher's algorithm은 짝수는 판단할 수 없기에 문자열 중간에 판단할 수 있는 문자를 삽입하여 짝수도 판단가능
   s = [].join.call(s, "#");
   s = "$#" + s + "#$";
 
+  // 각 A의 원소 A[i]는 i번째 문자열을 중심으로 하는 가장 긴 팔린드롬의 반지름의 길이.
+  // 즉, S[(i−A[i])⋯(i+A[i])]는 팔린드롬이며, S[(i−A[i]−1)⋯(i+A[i]+1)]은 팔린드롬이 아니다.
   var A = [],
     C = 1,
     R = 1,
