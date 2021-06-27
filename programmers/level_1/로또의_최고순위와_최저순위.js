311da9736a9b0f0174dc7fc4b987b62a5ec93250
swanious <ohsuwan1@gmail.com>
2021-06-27 21:36:15 +0900

    level1 - 로또의 최고순위와 최저순위 (filter, reduce로 값 누적하기)

--- programmers/level_1/로또의_최고순위와_최저순위.js
+++ programmers/level_1/로또의_최고순위와_최저순위.js
@@ -1,7 +1,17 @@
 function solution(lottos, win_nums) {
   var answer;
   var rank = { 6: 1, 5: 2, 4: 3, 3: 4, 2: 5, 1: 6, 0: 6 };
-  var zeroCnt = lottos.filter((v) => v === 0).length;
+  /** 배열에서 문자열, 숫자의 개수 구하기
+   *
+   * filter, reduce 사용
+   *
+   * filter - var zeroCnt = lottos.filter((v) => v === 0).length;
+   * reduce - var zeroCnt = lottos.reduce((accumulate, current) => (accumulate + current === 0), 0)
+   */
+  var zeroCnt = lottos.reduce(
+    (accumulate, current) => accumulate + (current === 0),
+    0
+  );
   var cnt = 0;
 
   // 일치하는 숫자의 개수 구하기
