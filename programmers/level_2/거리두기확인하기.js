7d369b9f684b0a56dfc2d19c2c64832e69e1d409
swanious <ohsuwan1@gmail.com>
2021-08-16 19:36:28 +0900

    level2 - 거리두기 확인하기(js) solve

--- programmers/level_2/거리두기확인하기.js
+++ programmers/level_2/거리두기확인하기.js
@@ -1,12 +1,12 @@
 function solution(places) {
   var answer = [];
   // 방문 체크 배열
-  const visit = Array.from(Array(5), () => Array(5).fill(true));    
-  
-  places.forEach(eachMap => {
-      bfs(eachMap) ? answer.push(1) : answer.push(0)
-  })
-  return answer
+  const visit = Array.from(Array(5), () => Array(5).fill(true));
+
+  places.forEach((eachMap) => {
+    bfs(eachMap) ? answer.push(1) : answer.push(0);
+  });
+  return answer;
 }
 
 const bfs = (arr) => {
@@ -16,34 +16,41 @@ const bfs = (arr) => {
   let check = false;
 
   posP.forEach(([r, c]) => {
-      const q = [[r, c]];
-      const v = Array.from(Array(5), () => Array(5).fill(false));
-      
-      while (q.length !== 0) {
-          let [row, col] = q.shift();
-          v[row][col] = true
-          for (let i = 0; i < 3; i++) {
-              const nr = row + dr[i];
-              const nc = col + dc[i];
-              if (nr < 0 || nc < 0 || nr > 4 || nc > 4 || v[nr][nc]) break;
-              if (arr[nr][nc] === 'X') continue;
-              else if (arr[nr][nc] === 'O' && Math.abs(nr - r) + Math.abs(nc - c) <= 1) q.push([nr, nc]);
-              else if (arr[nr][nc] === 'P' && Math.abs(nr - r) + Math.abs(nc - c) <= 2) {
-                  check = true;
-              }
-          }
+    const q = [[r, c]];
+    const v = Array.from(Array(5), () => Array(5).fill(false));
+
+    while (q.length !== 0) {
+      let [row, col] = q.shift();
+      v[row][col] = true;
+      for (let i = 0; i < 3; i++) {
+        const nr = row + dr[i];
+        const nc = col + dc[i];
+        if (nr < 0 || nc < 0 || nr > 4 || nc > 4 || v[nr][nc]) continue;
+        if (arr[nr][nc] === "X") continue;
+        else if (
+          arr[nr][nc] === "O" &&
+          Math.abs(nr - r) + Math.abs(nc - c) <= 1
+        )
+          q.push([nr, nc]);
+        else if (
+          arr[nr][nc] === "P" &&
+          Math.abs(nr - r) + Math.abs(nc - c) <= 2
+        ) {
+          check = true;
+        }
       }
-  })
+    }
+  });
   return check ? false : true;
-}
+};
 
 const getP = (arr) => {
   let temp = [];
   for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-          if (arr[i][j] === 'P') temp.push([i, j])
-      }
+    for (let j = 0; j < 5; j++) {
+      if (arr[i][j] === "P") temp.push([i, j]);
+    }
   }
-  
+
   return temp;
-}
+};
