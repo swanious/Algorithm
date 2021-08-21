2ace30c660cf37dec657b22dc828892cccf10bbb
swanious <ohsuwan1@gmail.com>
2021-08-21 23:58:50 +0900

    comment: level3 - 정수삼각형(python)/dp

--- programmers/level_3/정수삼각형.py
+++ programmers/level_3/정수삼각형.py
@@ -2,10 +2,13 @@ def solution(triangle):
     n = len(triangle)
     for i in range(1, n):
         for j in range(i + 1):
+            # 맨 왼쪽의 값
             if j==0:
                 triangle[i][j] += triangle[i-1][j]
+            # 맨 오른쪽의 값
             elif j == i:
                 triangle[i][j] += triangle[i-1][j-1]
+            # 중간의 값들은 이전 행의 두개의 값 중 큰 값을 저장
             else:
                 triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
             
