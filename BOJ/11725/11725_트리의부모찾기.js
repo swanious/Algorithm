b13a9806ed570499225298a0f5f6a081e46553d7
swanious <ohsuwan1@gmail.com>
2021-09-12 02:35:47 +0900

    fix: change i/o

--- BOJ/11725/11725_트리의부모찾기.js
+++ BOJ/11725/11725_트리의부모찾기.js
@@ -1,15 +1,3 @@
-/**
-shift연산
- */
-
-const fs = require('fs')
-const stdin = (fs.readFileSync('tc.txt').toString()).split('\n')
-// const stdin = (fs.readFileSync('/dev/stdin').toString()).split('\n')
-
-const input = (() => {
-  let line = 0;
-  return () => stdin[line++]
-})()
 class Node {
   constructor(item) {
     this.item = item;
@@ -49,29 +37,39 @@ class Q {
   }
 }
 
-const V = +input();
-const adj = Array.from(Array(V + 1), () => []);
-const visit = new Array(V + 1).fill(false);
-for (let i = 0; i < V-1; i++) {
-  const [from, to] = input().split(' ').map(Number);
-  adj[from].push(to);
-  adj[to].push(from);
-}
+const input = [];
+const strToNum = (str) => str.split(' ').map(Number);
 
-const q = new Q();
-let answer = new Array(V + 1);
-answer[1] = 0;
-q.append(1);
-visit[1] = true;
-while (!q.isEmpty()) {
+require('readline')
+  .createInterface(process.stdin, process.stdout)
+  .on('line', (line) => {
+    input.push(line)
+  })
+  .on('close', () => {
+    const V = +input.shift();
+    const adj = Array.from(Array(V + 1), () => []);
+    const visit = new Array(V + 1).fill(false);
+    input.forEach(v => {
+      const [from, to] = strToNum(v);
+      adj[from].push(to);
+      adj[to].push(from);
+    })
 
-  const v = q.popleft();
-  for (let w of adj[v]) {
-    if (!visit[w]) {
-      visit[w] = true;
-      q.append(w);
-      answer[w] = v;
+    const q = new Q();
+    let answer = new Array(V + 1);
+    answer[1] = 0;
+    q.append(1);
+    visit[1] = true;
+    while (!q.isEmpty()) {
+    
+      const v = q.popleft();
+      for (let w of adj[v]) {
+        if (!visit[w]) {
+          visit[w] = true;
+          q.append(w);
+          answer[w] = v;
+        }
+      }
     }
-  }
-}
-answer.forEach((v, i) => { if (i > 1) console.log(v) })
+    answer.forEach((v, i) => { if (i > 1) console.log(v) })
+  })
