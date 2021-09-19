781a3a9e868845b679fde0752fa0ac8b08a3f2bb
swanious <ohsuwan1@gmail.com>
2021-09-20 00:05:51 +0900

    fix: Add comment for explaination

--- programmers/level_3/N으로표현.py
+++ programmers/level_3/N으로표현.py
@@ -1,16 +1,15 @@
 def solution(N, number):
-    answer = 0
     new_set = [set()] * 8
     
     for i in range(8):
         new_set[i].add(int(N * (i + 1)))
         for j in range(0, i):
-            for arg1 in set[i]:
-                for arg2 in set[i-j-1]:
+            for arg1 in new_set[i]:
+                for arg2 in new_set[i-j-1]:
                     new_set[i].add(arg1+arg2)
                     new_set[i].add(arg1-arg2)
                     new_set[i].add(arg1*arg2)
                     new_set[i].add(arg1//arg2)
                     
-        if new_set[i].has(number): return i + 1
+        if number in new_set[i]: return i + 1
     return -1
